48ded2d8c7cba6324a64ac76152d02abb5c81f0a
Peter Jones <pwjones@cnbc.cmu.edu>
2012-09-27 16:54:36 -0400

    First commit
    Signed-off-by: Peter Jones <pwjones@cnbc.cmu.edu>

---
+++ addons/ofxFlea3/ofxFlea3.cpp
@@ -0,0 +1,568 @@
+
+#include "ofxFlea3.h"
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace FlyCapture2;
+
+ofxFlea3::ofxFlea3()
+{
+    fcCameraID=0;
+    //First assume there are MAX_CAMS.Then this variable is modified to the
+    //true camera number after listDevices() is called.
+    camNum=_MAX_CAMS;
+	
+
+}
+
+void PrintError( Error error)                                                                                                                     
+{
+    error.PrintErrorTrace();
+}
+
+void ofxFlea3::listDevices()
+{
+    printf("The following are Flea3 devices\n");
+    // This function enumerates all the cameras found on the machine, across
+    // all 1394 buses and cards. 
+	BusManager busMgr;
+	Error error;
+    unsigned int numCameras;
+    error = busMgr.GetNumOfCameras(&numCameras);
+    if (error != PGRERROR_OK)
+    {
+        PrintError(error);
+        return;
+    }
+
+	if ( numCameras < 1 )
+    {
+       printf( "No camera detected.\n" );
+       return;
+    } else {
+       printf( "Number of cameras detected: %u\n", numCameras );
+    }
+	camNum = numCameras;
+	printf( "Getting GUIDs for the camera(s)\n");
+	for (unsigned int i=0; i<numCameras; i++) {
+		error = busMgr.GetCameraFromIndex(0, &pgrGuid[i]);
+		if (error != PGRERROR_OK) {
+			PrintError(error);
+			return;
+		}
+	}
+
+}
+
+void ofxFlea3::setDeviceID(int id)
+{
+    fcCameraID=id;
+}
+int ofxFlea3::getDeviceID()
+{
+    return fcCameraID;
+}
+void ofxFlea3::initFlea3(int wid,int hei,int startX,int startY)
+{
+	Error error;
+	FC2Config config;
+	const Mode k_fmt7Mode = MODE_0;
+    const PixelFormat k_fmt7PixFmt = PIXEL_FORMAT_MONO8;
+    //Camera *cam;
+	listDevices();
+    for(int i=0;i<camNum;i++) {
+		//
+		// Create the cams. This call must be made before any other calls to
+		// to the cams are made, as a valid cams is needed for all
+		// camera-specific FlyCapture library calls. This call sets the cams
+		// to the default settings, but flycaptureInitialize() must be called
+		// below to fully initialize the camera for use.
+		
+		// Newer OO way to construct a camera object and initialize   
+		cams[i].Connect( &pgrGuid[i] ); 
+		
+		// Get and display the camera information
+		CameraInfo camInfo;
+		error = cams[i].GetCameraInfo(&camInfo);
+		if (error != PGRERROR_OK)
+		{
+			PrintError(error);
+			return;
+		}
+		PrintCameraInfo(&camInfo);
+		
+		guid.Data1 = pgrGuid[1].value[1]; // This assigns the first part of the point grey specific GUID to the OF one.
+
+		// Initialize the camera. This call initializes one of the cameras on the
+		// bus with the FlyCapturecams that is passed in. This should generally
+		// be called right after creating the cams but before doing anything
+		// else.
+		//
+		// This call performs several functions. It sets the camera to communicate
+		// at the proper bus speed, turns on color processing (if available) and
+		// sets the Bayer orientation to the correct setting. Finally, it also
+		// initializes the white balance tables for cameras that support that
+		// function.
+		//
+		// Start grabbing images in the current videomode and framerate. Driver
+		// level image buffer allocation is performed at this point. After this
+		// point, images will be constantly grabbed by the camera and stored
+		// in the buffers on the PC.
+		// Start capturing images
+		printf("Getting camera configuration\n");
+		error = cams[i].GetConfiguration(&config);
+		if (error != PGRERROR_OK) {
+			printf("There is a config error: ofxFlea3.cpp:111\n");
+			PrintError(error);
+
+			return;
+		}
+		
+		// Configuring embedded information to include a timestamp
+		//Get embedded information
+		EmbeddedImageInfo info;
+		error = cams[i].GetEmbeddedImageInfo(&info);
+		//Set embedded timestamp
+		info.timestamp.onOff = true;
+		//Set embedded information
+		error = cams[i].SetEmbeddedImageInfo(&info);
+		
+		// Format 7 is the custom video mode that allows "non-standard" resolutions and frame rates.
+		// Must use format 7 specific methods
+		// Query for available Format 7 modes
+
+		//Format7Info fmt7Info;
+		//bool supported;
+		//
+		//fmt7Info.mode = k_fmt7Mode;
+		//error = cams[i].GetFormat7Info( &fmt7Info, &supported );
+		//if (error != PGRERROR_OK) {
+		//	PrintError( error );
+		//	return;
+		//}
+		//Format7ImageSettings fmt7ImageSettings;
+		//fmt7ImageSettings.mode = k_fmt7Mode;
+		//fmt7ImageSettings.offsetX = startX;
+		//fmt7ImageSettings.offsetY = startY;
+		//fmt7ImageSettings.width = wid;
+		//fmt7ImageSettings.height = hei;
+		//fmt7ImageSettings.pixelFormat = k_fmt7PixFmt;
+
+		//bool valid;
+		//Format7PacketInfo fmt7PacketInfo;
+		//// Validate the settings to make sure that they are valid
+		//error = cams[i].ValidateFormat7Settings(&fmt7ImageSettings, &valid, &fmt7PacketInfo );
+		//if (error != PGRERROR_OK) {
+		//	PrintError( error );
+		//	return;
+		//}
+		//if ( !valid ) {
+		//	// Settings are not valid
+		//	printf("Format7 settings are not valid\n");
+		//	return;
+		//}
+
+		// Now set the frame rate
+		//VideoMode videoMode;
+		//FrameRate frameRate;
+		//error = cams[i].GetVideoModeAndFrameRate(&videoMode, &frameRate);
+		//if (error != PGRERROR_OK) { PrintError( error ); return; }
+		//double rate = getRequestedFrameRate();
+		////frameRate = FRAMERATE_60;
+		//error = cams[i].GetVideoModeAndFrameRateInfo(videoMode, frameRate, &supported);
+		//if (error != PGRERROR_OK) { PrintError( error ); return; }
+		//if (supported) {
+		//	error = cams[i].SetVideoModeAndFrameRate(videoMode, frameRate);
+		//	if (error != PGRERROR_OK) { PrintError( error ); return; }
+		//} else {
+		//	printf("The video frame rate and mode specified are not supported.  Exiting.\n");
+		//	return;
+		//}
+		// Set the settings to the camera
+		//error = cams[i].SetFormat7Configuration(&fmt7ImageSettings, fmt7PacketInfo.recommendedBytesPerPacket);
+		//if (error != PGRERROR_OK) { PrintError( error ); return; }
+		
+
+		printf( "Starting capture... \n" );
+		error = cams[i].StartCapture();
+		if (error != PGRERROR_OK) {
+			printf("There is a capture error: ofxFlea3.cpp:112\n");
+			PrintError(error);
+			return;
+		} else {
+			printf("Capture started successfully\n");
+		}
+		Image rawImage;
+		for (int ii = 0; ii<5; ii++) {
+			error = cams[i].RetrieveBuffer(&rawImage);
+			if (error != PGRERROR_OK){
+				printf("Error grabbing image: ofxFlea3.cpp:122 %u\n", ii);
+				continue;
+			} else {
+				//Print Timestamp value
+				printf( "Grabbed image %d\n", ii );
+			}
+			printf("Copying image\n");
+			fcImage[i].DeepCopy(&rawImage);
+			printf("Converting Image");
+			Image convIm;
+			error = rawImage.Convert( PIXEL_FORMAT_MONO8, &convIm);
+			convIm.Save("FleaImage_init.bmp");
+		}
+
+    }//end of for loop
+}
+void ofxFlea3::cameraInitializationLogic()
+{
+	int wid = 1280, hei = 1024, startX = 0, startY = 0;
+
+	initFlea3(wid,hei,startX,startY);
+}
+void ofxFlea3::cameraDeinitializationLogic()
+{
+	Error error;
+
+	for(int i=0;i<camNum;i++){
+		printf("Stopping capture\n");
+		cams[i].StopCapture();
+		// Disconnect the camera
+		error = cams[i].Disconnect();
+		if (error != PGRERROR_OK) {
+			PrintError(error);
+		}
+	}
+}
+CAMERA_BASE_FEATURE* ofxFlea3::getSupportedFeatures(int* featuresCount)
+{
+	*featuresCount = 7;
+	CAMERA_BASE_FEATURE* features = (CAMERA_BASE_FEATURE*)malloc(*featuresCount * sizeof(CAMERA_BASE_FEATURE));
+	features[0] = BASE_BRIGHTNESS;
+	features[1] = BASE_EXPOSURE;
+	features[2] = BASE_SATURATION;
+	features[3] = BASE_GAMMA;
+	features[4] = BASE_SHUTTER;
+	features[5] = BASE_GAIN;
+	features[6] = BASE_FRAMERATE;
+	return features;
+}
+void ofxFlea3::setCameraFeature(CAMERA_BASE_FEATURE featureCode,int firstValue,int secondValue,bool isAuto,bool isEnabled)
+{
+	Property deviceProperty;
+	deviceProperty.type = UNSPECIFIED_PROPERTY_TYPE;
+	switch (featureCode)
+	{
+	case BASE_BRIGHTNESS:
+		deviceProperty.type = BRIGHTNESS;
+		break;
+	case BASE_EXPOSURE:
+		deviceProperty.type = AUTO_EXPOSURE;
+		break;
+	case BASE_SATURATION:
+		deviceProperty.type = SATURATION;
+		break;
+	case BASE_GAMMA:
+		deviceProperty.type = GAMMA;
+		break;
+	case BASE_SHUTTER:
+		deviceProperty.type = SHUTTER;
+		break;
+	case BASE_GAIN:
+		deviceProperty.type = GAIN;
+		break;
+	case BASE_FRAMERATE:
+		deviceProperty.type = FRAME_RATE;
+		framerate = firstValue;
+		break;
+	}
+	if (deviceProperty.type != UNSPECIFIED_PROPERTY_TYPE) {
+		deviceProperty.onOff = isEnabled;
+		deviceProperty.valueA = firstValue;
+		deviceProperty.valueB = secondValue;
+		deviceProperty.autoManualMode = isAuto;
+		for (int i=0; i<camNum; i++) {
+			Error error = cams[i].SetProperty(&deviceProperty);
+			if (error != PGRERROR_OK) {
+				std::cout << "There was an error setting camera properties\n";
+			}
+		}
+	}
+}
+
+void ofxFlea3::getCameraFeature(CAMERA_BASE_FEATURE featureCode,int* firstValue,int* secondValue, bool* isAuto, bool* isEnabled,int* minValue,int* maxValue)
+{
+	Property deviceProperty;
+	deviceProperty.type = UNSPECIFIED_PROPERTY_TYPE;
+	*firstValue = 0;
+	*secondValue = 0;
+	*isAuto = false;
+	*isEnabled = false;
+	*minValue = 0;
+	*maxValue = 0;
+	switch (featureCode) {
+		case BASE_BRIGHTNESS:
+			deviceProperty.type = BRIGHTNESS;
+			break;
+		case BASE_EXPOSURE:
+			deviceProperty = AUTO_EXPOSURE;
+			break;
+		case BASE_SATURATION:
+			deviceProperty = SATURATION;
+			break;
+		case BASE_GAMMA:
+			deviceProperty = GAMMA;
+			break;
+		case BASE_SHUTTER:
+			deviceProperty = SHUTTER;
+			break;
+		case BASE_GAIN:
+			deviceProperty = GAIN;
+			break;
+		case BASE_FRAMERATE:
+			deviceProperty = FRAME_RATE;
+			break;
+	}
+	if (deviceProperty.type != UNSPECIFIED_PROPERTY_TYPE) {
+		Error error = cams[1].GetProperty(&deviceProperty); //This is obviously only working for the first camera, if multiple are connected.
+		if (error != PGRERROR_OK) {
+			std::cout << "There was an error getting camera properties\n";
+		} else {
+			*firstValue = (int) deviceProperty.valueA;
+			*secondValue = (int) deviceProperty.valueB;
+			*isAuto = deviceProperty.autoManualMode;
+			*isEnabled = deviceProperty.onOff;
+			*minValue = 0;
+			*maxValue = (int) deviceProperty.valueA;
+		}
+	}
+}
+
+void ofxFlea3::getNewFrame(unsigned char* newFrame)
+{
+	// Note: need to put an option to get a timestamp for the images.
+	Image rawImage, imCopy;
+	Error error;
+	unsigned int dsize;
+	error = cams[fcCameraID].RetrieveBuffer(&rawImage);
+    if (error != PGRERROR_OK)
+    {
+		printf("Error grabbing image %u\n", fcCameraID);
+    }
+	dsize = rawImage.GetDataSize();
+	memcpy((void *)newFrame, (const unsigned char*)rawImage.GetData(), dsize);
+	TimeStamp raw_ts = rawImage.GetTimeStamp(); 
+	timestamp = ofxTimeStamp(raw_ts); //construct the timestamp using the PRG TimeStamp
+	// Get some image information
+	//PixelFormat pixFormat;
+    //unsigned int rows=1024, cols=1280, stride, dsize;
+    //rawImage.GetDimensions( &rows, &cols, &stride, &pixFormat );
+	// Create a converted image
+    //Image convertedImage;
+	// Convert the raw image
+	//error = rawImage.Convert( PIXEL_FORMAT_MONO8, &convertedImage );
+    //if (error != PGRERROR_OK) {
+	//	PrintError( error );
+	//	cout << "Error in getNewFrame, conversion\n";
+    //}
+	rawImage.ReleaseBuffer();
+	//dsize = convertedImage.GetDataSize();
+	//convertedImage.Save("camImage_ofxFlea3.bmp");
+	//unsigned char *tempIm = (unsigned char *)malloc(dsize);
+	//memcpy((void *)newFrame, (const unsigned char*)convertedImage.GetData(), dsize);
+	
+	
+	// Image saving to debug
+	//Image newIm(height, width, width, tempIm, dsize, FlyCapture2::PIXEL_FORMAT_MONO8);
+	//newIm.Save("FleaTemp_image.bmp");
+	//memcpy((void *)newFrame, (const void*)tempIm, dsize);
+	//convertedImage.ReleaseBuffer();
+	//free(tempIm);
+}
+
+void ofxFlea3::getNewFrame(Image *newFrame)
+{
+	// This is the version that includes all of the niceties of the Image object provided
+	// by the FlyCapture2 library. Always returns the image in 8-bit Grayscale format. The 
+	// function expects the newFrame image to exist.
+	Image rawImage, imCopy;
+	Error error;
+	error = cams[fcCameraID].RetrieveBuffer(&rawImage);
+    if (error != PGRERROR_OK)
+    {
+		printf("Error grabbing image %u\n", fcCameraID);
+    }
+	// Get some image information
+	PixelFormat pixFormat;
+    unsigned int rows, cols, stride, dsize;
+    TimeStamp raw_ts;
+	rawImage.GetDimensions( &rows, &cols, &stride, &pixFormat );
+	raw_ts = rawImage.GetTimeStamp();
+
+	// Create a converted image
+    Image convertedImage;
+	error = rawImage.Convert( PIXEL_FORMAT_MONO8, newFrame );
+    if (error != PGRERROR_OK) {
+		PrintError( error );
+    }
+	//Now check the timestamp of the original image and the converted.  Note, the 
+	// converted images are no longer timestamped.
+	TimeStamp conv_ts;
+	conv_ts = convertedImage.GetTimeStamp();
+	printf("Raw TS: %d sec ", raw_ts.seconds);
+	printf("%d usec\n", raw_ts.microSeconds);
+	//printf("--- Conv TS: %d sec %d usec\n", conv_ts.seconds, conv_ts.microSeconds);
+	//rawImage.DeepCopy(&imCopy);
+	//dsize = convertedImage.GetDataSize();
+	//memcpy(newFrame, convertedImage.GetData(), dsize);
+	//newFrame = convertedImage.GetData();
+	newFrame->Save("camImage_ofxFlea3_imgv.bmp");
+	rawImage.ReleaseBuffer();
+}
+
+void ofxFlea3::getNewFrame(Image *newFrame, TimeStamp *ts)
+{
+	// This is the version that includes all of the niceties of the Image object provided
+	// by the FlyCapture2 library. Always returns the image in 8-bit Grayscale format. The 
+	// function expects the newFrame image to exist.  The timestamp input is there in order
+	// for the function to be able to return that information, which is lost after calling
+	// Image.Convert
+	Image rawImage, imCopy;
+	Error error;
+	error = cams[fcCameraID].RetrieveBuffer(&rawImage);
+    if (error != PGRERROR_OK)
+    {
+		printf("Error grabbing image %u\n", fcCameraID);
+    }
+	// Get some image information
+	PixelFormat pixFormat;
+    unsigned int rows, cols, stride, dsize;
+    TimeStamp raw_ts;
+	rawImage.GetDimensions( &rows, &cols, &stride, &pixFormat );
+	raw_ts = rawImage.GetTimeStamp();
+	*ts = raw_ts;
+	// Create a converted image
+    Image convertedImage;
+	error = rawImage.Convert( PIXEL_FORMAT_MONO8, newFrame );
+    if (error != PGRERROR_OK) {
+		PrintError( error );
+    }
+	//Now check the timestamp of the original image and the converted
+	TimeStamp conv_ts;
+	conv_ts = convertedImage.GetTimeStamp();
+	printf("Raw TS: %d sec ", raw_ts.seconds);
+	printf("%d usec\n", raw_ts.microSeconds);
+	printf("--- Conv TS: %d sec %d usec\n", conv_ts.seconds, conv_ts.microSeconds);
+	//rawImage.DeepCopy(&imCopy);
+	//dsize = convertedImage.GetDataSize();
+	//memcpy(newFrame, convertedImage.GetData(), dsize);
+	//newFrame = convertedImage.GetData();
+	newFrame->Save("camImage_ofxFlea3_imgv.bmp");
+	rawImage.ReleaseBuffer();
+
+}
+
+int ofxFlea3::getDeviceCount()
+{
+	// Get number of PGRCameras
+	int numCams;
+	numCams = camNum;
+	return numCams;
+}
+int ofxFlea3::getCamWidth()
+{
+	unsigned int width;
+
+    width=fcImage[fcCameraID].GetCols();
+    return width;
+}
+int ofxFlea3::getCamHeight()
+{
+	unsigned int height;
+
+    height=fcImage[fcCameraID].GetRows();
+    return height;
+}
+void ofxFlea3::setCameraType()
+{
+	cameraType = FLEA;
+	cameraTypeName = "FLEA";
+}
+
+double ofxFlea3::getRequestedFrameRate()
+{
+	// Set up the frame rate data
+    Property prop;
+    prop.type = FRAME_RATE;
+    if ( &cams[1] == NULL) {
+        return 0.0;
+    }
+    else {
+	    Error error = cams[1].GetProperty( &prop );
+        return (error == PGRERROR_OK) ? prop.absValue : 0.0;
+    }
+}
+
+void ofxFlea3::PrintCameraInfo( CameraInfo* pCamInfo )
+{
+	std::printf(
+        "\n*** CAMERA INFORMATION ***\n"
+        "Serial number - %u\n"
+        "Camera model - %s\n"
+        "Camera vendor - %s\n"
+        "Sensor - %s\n"
+        "Resolution - %s\n"
+        "Firmware version - %s\n"
+        "Firmware build time - %s\n\n",
+        pCamInfo->serialNumber,
+        pCamInfo->modelName,
+        pCamInfo->vendorName,
+        pCamInfo->sensorInfo,
+        pCamInfo->sensorResolution,
+        pCamInfo->firmwareVersion,
+        pCamInfo->firmwareBuildTime );
+}
+
+//void ofxFlea3::setCameraFeature(CAMERA_BASE_FEATURE featureCode,int firstValue,int secondValue,bool isAuto,bool isEnabled)
+//{
+//}
+//void ofxFlea3::getCameraFeature(CAMERA_BASE_FEATURE featureCode,int* firstValue,int* secondValue, bool* isAuto, bool* isEnabled,int* minValue,int* maxValue)
+//{
+//}
+int ofxFlea3::getCameraBaseCount()
+{
+	listDevices();
+	return camNum;
+}
+GUID* ofxFlea3::getBaseCameraGuids(int* camCount)
+{
+	listDevices();
+	*camCount = camNum;
+	GUID* guids = (GUID*)malloc(*camCount * sizeof(GUID));
+	for (int i=0;i<*camCount;i++)
+	{
+		GUID tempGuid;
+
+		tempGuid.Data1 = i;
+		tempGuid.Data2 = tempGuid.Data3 = 0;
+		memset(tempGuid.Data4,0,8*sizeof(unsigned char));
+		guids[i] = tempGuid;
+	}
+	return guids;
+}
+//CAMERA_BASE_FEATURE* ofxFlea3::getSupportedFeatures(int* featuresCount)
+//{
+//	CAMERA_BASE_FEATURE *features = NULL;
+//	return features;
+//}
+void ofxFlea3::callSettingsDialog()
+{
+}
+
+
+//Clean up
+ofxFlea3::~ofxFlea3()
+{
+	
+}
+
---
+++ addons/ofxFlea3/ofxFlea3.h
@@ -0,0 +1,55 @@
+#ifndef OFXFLEA3_H_INCLUDED
+#define OFXFLEA3_H_INCLUDED
+
+#include "stdio.h"
+#include "ofxCameraBase.h"
+#include "FlyCapture2.h"
+#include "Image.h"
+
+using namespace FlyCapture2;
+//
+// The maximum number of cameras on the bus.
+//
+#define _MAX_CAMS   10
+
+class ofxFlea3 : ofxCameraBase
+{
+    public:
+        ofxFlea3();
+		~ofxFlea3();
+        void initFlea3(int wid,int hei,int startX=0,int startY=0);
+        void setDeviceID(int id);
+        int getDeviceID();
+        int getCamWidth();
+        int getCamHeight();
+        void listDevices();
+        int getDeviceCount();
+		void getNewFrame(unsigned char* newFrame);
+		void getNewFrame(Image *newFrame);
+		void getNewFrame(Image *newFrame, TimeStamp *ts);
+		GUID* getBaseCameraGuids(int* camCount);
+		// Not implemented yet
+		void setCameraFeature(CAMERA_BASE_FEATURE featureCode,int firstValue,int secondValue,bool isAuto,bool isEnabled);
+		void getCameraFeature(CAMERA_BASE_FEATURE featureCode,int* firstValue,int* secondValue, bool* isAuto, bool* isEnabled,int* minValue,int* maxValue);
+		int getCameraBaseCount();
+		CAMERA_BASE_FEATURE* getSupportedFeatures(int* featuresCount);
+		void callSettingsDialog();
+	protected:
+		void setCameraType();
+		void cameraInitializationLogic();
+		void cameraDeinitializationLogic();
+    private:
+        // This acts as a handle to the camera.
+		Camera cams[_MAX_CAMS];
+		int fcCameraID;
+        int camWidth;
+        int camHeight;
+		int camNum ;
+        // Enumerate the cameras on the bus.
+		PGRGuid pgrGuid[_MAX_CAMS];
+		Image fcImage[_MAX_CAMS]; //This Image object is from the FlyCapture2 lib
+		// Private functions
+		void PrintCameraInfo( CameraInfo* pCamInfo );
+		double ofxFlea3::getRequestedFrameRate();
+};
+#endif // OFXFLEA3_H_INCLUDED
