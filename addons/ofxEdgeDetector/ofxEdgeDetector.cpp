f1058606f079b5f33730a86aa098c73ec45e85bf
Peter Jones <pwjones76@gmail.com>
2014-08-05 16:19:13 -0400

    First commit with edge detector.  Does not work.  Thinking about switching CCV to openCV 2

---
+++ addons/ofxEdgeDetector/ofxEdgeDetector.cpp
@@ -0,0 +1,43 @@
+
+#include "ofxEdgeDetector.h"
+
+
+ofxEdgeDetector::ofxEdgeDetector() {
+	printf("Hi, what's up? I'm edge detector\n");
+}
+
+ofxEdgeDetector::~ofxEdgeDetector()
+{
+}
+
+void ofxEdgeDetector::detectEdges()
+{
+	IplImage* img = cvLoadImage( "my3.jpg", 0);
+	cvNamedWindow( "example-input" );
+	cvNamedWindow( "example-output" );
+
+	// Show the original image
+	cvShowImage("example-input", img);
+
+	// Make sure image is divisible by 2
+	assert( img->width%2 == 0 && img->height%2 == 0);
+
+	// Create an image for the output
+	IplImage* out = cvCreateImage( cvSize(img->width/2,img->height/2), img->depth, img->nChannels );
+
+	// Reduce the image by 2
+	cvPyrDown( img, out );
+
+	// Perform canny edge detection
+	cvCanny( out, out, 10, 100, 3 );
+
+	// Show the processed image
+	cvShowImage("example-output", out);
+
+	cvWaitKey(0);
+	cvReleaseImage( &img );
+	cvReleaseImage( &out );
+	cvDestroyWindow( "example-input" );
+	cvDestroyWindow( "example-output" );
+ 
+ }
---
+++ addons/ofxEdgeDetector/ofxEdgeDetector.h
@@ -0,0 +1,16 @@
+#ifndef OFXEDGEDETECTOR_H
+#define OFXEDGEDETECTOR_H
+
+#include "ofxOpenCv.h"
+#include "ofxFC2MovieWriter.h"
+
+class ofxEdgeDetector
+{
+public:
+	ofxEdgeDetector();
+	~ofxEdgeDetector();
+
+	void detectEdges();
+}
+
+#endif // OFXEDGEDETECTOR_H
